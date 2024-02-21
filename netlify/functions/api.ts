3f2c2eef3551f71d5fdd3e05d06a9ac6e346a4dd
JoseA <joseantanaper@gmail.com>
2024-02-21 18:34:07 +0100

    First commit

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -3,6 +3,10 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
+const port = 3000
 const api = express();
 
 const router = Router();
@@ -10,7 +14,12 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
-// Avoid [[redirects]] section in netlify.toml
-// api.use("/.netlify/functions/api/:splat/", router);
-
 export const handler = serverless(api);
+
+if(process.env.MODE === 'development') {
+api.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`)
+})
+} else {
+  console.log(`[server]: Production mode`);
+}
