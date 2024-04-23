d5482da3d7a2cf38a2f2137688955dc53ea59c28
JoseA <joseantanaper@gmail.com>
2024-04-23 20:23:48 +0200

    *

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -20,20 +20,28 @@ const allowedDomains = [
   'https://iridescent-tapioca-b1d2f4.netlify.app/',
 ]
 
-app.use(
-  cors({
-    origin: function (origin, callback) {
-      // bypass the requests with no origin (like curl requests, mobile apps, etc )
-      if (!origin) return callback(null, true)
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       // bypass the requests with no origin (like curl requests, mobile apps, etc )
+//       if (!origin) return callback(null, true)
 
-      if (allowedDomains.indexOf(origin) === -1) {
-        var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`
-        return callback(new Error(msg), false)
-      }
-      return callback(null, true)
-    },
-  })
-)
+//       if (allowedDomains.indexOf(origin) === -1) {
+//         var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`
+//         return callback(new Error(msg), false)
+//       }
+//       return callback(null, true)
+//     },
+//   })
+// )
+
+const corsOptions = {
+  origin: '*',
+  credentials: true,
+  optionSuccessStatus: 200,
+}
+
+app.use(cors(corsOptions))
 
 // ROOT Router
 const router = Router()
